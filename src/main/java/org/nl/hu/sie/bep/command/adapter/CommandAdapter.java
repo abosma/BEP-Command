05ac4f1e6cbf33688a48c7d783c3fae4eb3ab7e4
Atilla Bosma <atilla.bosma@capgemini.com>
2019-06-23 14:07:42 +0200

    Begin gebruik van het Domein package via de adapter

--- src/main/java/org/nl/hu/sie/bep/command/adapter/CommandAdapter.java
+++ src/main/java/org/nl/hu/sie/bep/command/adapter/CommandAdapter.java
@@ -2,10 +2,12 @@ package org.nl.hu.sie.bep.command.adapter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.nl.hu.sie.bep.domain.adapter.*;
 
 public class CommandAdapter implements Adapter {
 
     private static Logger logger = LoggerFactory.getLogger(CommandAdapter.class);
+    private static CommandAdaptee commandAdaptee = new CommandAdaptee();
 
     private CommandAdapter(){
 
@@ -14,6 +16,6 @@ public class CommandAdapter implements Adapter {
     public static void createIEFFilesFromMonthNumber(int monthNumber) {
         String userLogInfo = String.format("Month %s was chosen.", monthNumber);
 
-        logger.info(userLogInfo);
+        commandAdaptee.createIEFFilesFromMonthNumber(monthNumber);
     }
 }
