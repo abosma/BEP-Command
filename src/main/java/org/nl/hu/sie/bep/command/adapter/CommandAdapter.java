3f8c6c676e65379bdea1548788c4f85d2e0b95b7
Atilla Bosma <atillabosma@hotmail.com>
2019-06-23 19:57:42 +0200

    Error handling voor het omzetten van data toegevoegd.

--- src/main/java/org/nl/hu/sie/bep/command/adapter/CommandAdapter.java
+++ src/main/java/org/nl/hu/sie/bep/command/adapter/CommandAdapter.java
@@ -1,9 +1,12 @@
 package org.nl.hu.sie.bep.command.adapter;
 
 import org.nl.hu.sie.bep.domain.adapter.CommandAdaptee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommandAdapter implements Adapter {
 
+    private static Logger logger = LoggerFactory.getLogger(CommandAdapter.class);
     private static CommandAdaptee commandAdaptee = new CommandAdaptee();
 
     private CommandAdapter(){
@@ -11,6 +14,10 @@ public class CommandAdapter implements Adapter {
     }
 
     public static void createIEFFilesFromMonthNumber(int monthNumber) {
-        commandAdaptee.createIEFFilesFromMonthNumber(monthNumber);
+        try {
+            commandAdaptee.createIEFFilesFromMonthNumber(monthNumber);
+        } catch (NullPointerException e) {
+            logger.info("Something went wrong with converting the data, please try again.");
+        }
     }
 }
